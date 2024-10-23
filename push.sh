0999b43356d09b746a7e7322f6480f8defdc2490
leapingturtlefrog <alexanderaridgides@gmail.com>
2024-10-22 22:07:20 -0400

    update push.sh

--- push.sh
+++ push.sh
@@ -1,3 +1,10 @@
+# Run by entering the command './push.sh "commit message"'
+# Adds, commits, pulls, merges, pushes, and then checks out
+# the branch you started on.
+#
+# May not work if there is a merge conflict, which would
+# then have to be handled separately.
+
 if [ $# -eq 1 ]; then
     git_status=$(git status)
     read -r branch_name <<< "$git_status"
@@ -10,7 +17,8 @@ if [ $# -eq 1 ]; then
     && git pull origin main \
     && git merge $branch_name \
     && git push -u origin main \
-    && (git checkout -b $branch_name &> /dev/null || git checkout $branch_name) \
+    && (git checkout -b $branch_name &> /dev/null \
+            || git checkout $branch_name) \
     && echo "Successful" || echo "Unsuccessful"
     
 else
