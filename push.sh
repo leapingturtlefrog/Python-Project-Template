cfbe86abc93f6f9c16d23b5c0d823531c2253fee
leapingturtlefrog <alexanderaridgides@gmail.com>
2024-10-23 16:21:15 -0400

    new git workflow

--- push.sh
+++ push.sh
@@ -12,20 +12,53 @@ if [ $# -eq 1 ]; then
     echo "$branch_name"
     branch_name=${branch_name:10}
     
+    if [ $branch_name -eq "main" ]; then
+        git add . \
+        && git commit -m "$1" \
+        && git pull origin main \
+        && git push -u origin main \
+        && echo "Script successful. Please checkout a non-main branch for the future" \
+        || echo "Script unsuccessful"
+    elif [ ${#branch_name} -gt 0 ]; then
+        git add . \
+        && git commit -m "$1" \
+        && git checkout main \
+        && git pull origin main \
+        && git merge "$branch_name" \
+        && git push -u origin main \
+        && echo "Push successful" \
+        && branch_letters=$(echo "$branch_name" | grep -o '[a-zA-Z]*') \
+        && branch_number=$(echo "$branch_name" | grep -o '[0-9]*') \
+        && new_branch_number=$((branch_number + 1)) \
+        && new_branch="$branch_letters$new_branch_number" \
+        && git checkout -b "$new_branch_name" \
+        && echo "Checkout successful" \
+        && echo "Original branch: $branch_name" \
+        && echo "New branch: $new_branch_name (checked out)" \
+        || echo "Script unsuccessful"
+    else
+        echo "Branch name not received from git status. Failure"
+    fi
+    : '
     if [ ${#branch_name} -gt 0 ]; then
         git add . \
         && git commit -m "$1" \
         && git checkout main \
         && git pull origin main \
-        && git merge $branch_name \
+        && git merge "$branch_name" \
         && git push -u origin main \
-        && (git checkout -b $branch_name &> /dev/null \
-                || git checkout $branch_name) \
-        && git merge main \
+        # && (git checkout -b "$branch_name" &> /dev/null \
+        #        || git checkout "$branch_name") \
+        # && 
         && echo "Script successful" || echo "Script unsuccessful"
+        
+        
+        $branch_name -eq "main" && echo "Please checkout a non-main branch for the future" \
+        || 
     else
         echo "Script unsuccessful. Current branch name was not read."
     fi
+    '
 else
     echo "Please enter the commit message as a single string argument."
 fi
